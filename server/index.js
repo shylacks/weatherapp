22b270a0c6299502d1a37d6aa23bff3f2216f676
Piotr Szylak <taskercio@gmail.com>
2021-04-17 00:15:24 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const Redis = require("ioredis");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+const dbConnDataMongo = {
+  host: process.env.MONGO_HOST || "my_mongo",
+  port: process.env.MONGO_PORT || 27017,
+  database: process.env.MONGO_DATABASE || "weathers",
+};
+
+const dbConnDataRedis = {
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || "my_redis",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const redis = new Redis(dbConnDataRedis);
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+mongoose
+  .connect(
+    `mongodb://${dbConnDataMongo.host}:${dbConnDataMongo.port}/${dbConnDataMongo.database}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((response) => {
+    console.log(
+      `Connected to MongoDB.\nDatabase name: "${response.connections[0].name}"`
+    );
+  })
+  .catch((error) => console.error("Error connecting to MongoDB", error));
+
+app.listen(PORT, () => {
+  console.log(`API server listening at http://localhost:${PORT}`);
+});
